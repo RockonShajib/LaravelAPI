9caccc8ab5b90bb94bab0e660659df28f4ee6c3c
Rockon <rockon3334@hotmail.com>
2019-03-10 16:48:49 +0600

    4mprint

---
+++ app/Model/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
+
+class Product extends Model
+{
+
+    protected $fillable = [
+        'name','detail','price','stock','discount','user_id'
+    ];
+   public function reviews(){
+       return $this->hasMany(Review::class);
+   }
+}
